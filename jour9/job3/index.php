7c07e56cfd65fdc1ed15a4812a6d616a8fa94a37
Enzo.Fraioli <enzo.fraioli@laplateforme.io>
2021-11-12 15:44:20 +0100

    runtrack2 jour9

---
+++ jour9/job3/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title>Cours PHP / MySQL</title>
+        <meta charset="utf-8">
+
+    </head>
+    <body>
+        <h1>Bases de données MySQL</h1>
+        <?php
+            $bdd= mysqli_connect("localhost","root","","jour8"); 
+
+            $req= mysqli_query($bdd,"SELECT prenom,nom,naissance FROM etudiants WHERE sexe='Femme'");
+
+            $res= mysqli_fetch_all($req, MYSQLI_ASSOC);
+            ?>
+        <h1>Tableau</h1>
+        <table>
+            <thead>
+                    <?php
+                    echo '<tr>';
+                    foreach($res[0] as $key => $value){
+
+                            echo "<th>$key</th>";
+                    }
+                    echo '</tr>';
+                    ?>
+            </thead>
+            <tbody>
+                <tr>
+            <?php
+                foreach($res as $key => $value){ 
+
+                    echo '<tr>';
+
+                    foreach ($value as $key1 => $value1) 
+                    {
+                        echo "<td>$value1</td>";
+                    }
+                        echo '</tr>'; 
+                }
+            ?>
+            </tbody>
+        </table>
+    </body>
+</html>
